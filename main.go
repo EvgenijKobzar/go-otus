8cd4584c4b321b1f7fa52396c466e69e4df21ede
Евгений Кобзарь <ekobzar@mts.ru>
2025-04-20 17:08:46 +0200

    Домашняя работа №3

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const NewLineChar = "\n"
 
 func main() {
-	fmt.Println("Hello World")
+
+	str8х8 := " # # # #\n" +
+		" # # # #\n" +
+		" # # # #\n" +
+		" # # # #\n"
+	fmt.Println(str8х8)
+
+	x := 16
+	y := 16
+	fmt.Println(render(x, y))
+}
+
+func render(x int, y int) string {
+	result := ""
+
+	for i := 0; i < y; i++ {
+		result += axisX(x) + NewLineChar
+	}
+	return result
+}
+
+func axisX(len int) string {
+	result := ""
+
+	for i := 0; i < len; i++ {
+		if i%2 == 0 {
+			result += " "
+		} else {
+			result += "#"
+		}
+	}
+
+	return result
 }
